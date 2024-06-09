4194653a13bb726f479d7ff402bd76918c0ce7c1
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-06-08 20:16:44 -0400

    Add 'category' field to CreatePost form

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,43 +1,82 @@
+import { useMemo } from 'react';
+import useFetch from '../utils/use-fetch';
 import Form from '../components/Form';
+import LoadingIndicator from '../components/LoadingIndicator';
 import { BASE_URL } from '../config';
-import { ApiResponse } from '../types';
+import { ApiResponse, CategoryData } from '../types';
 import extractErrorMsg from '../utils/extract-error-msg';
 
-const fields = [
-  {
-    name: 'title',
-    label: 'Title',
-    type: 'text',
-    placeholder: 'title of post',
-    required: true,
-    colSpan: true,
-  },
-  {
-    name: 'content',
-    label: 'Content',
-    type: 'editor',
-    placeholder: 'Enter post content here...',
-    required: true,
-    colSpan: true,
-  },
-  // publish checkbox
-  // category
-  // tags
-  // displayImg
-];
-
 export default function CreatePost() {
+  const { data, error, loading } = useFetch<ApiResponse<CategoryData[]>>(
+    `${BASE_URL}api/categories?sort[name]=asc`,
+  );
+
+  const fields = useMemo(() => {
+    return data ? getFields(data.data) : [];
+  }, [data]);
+
   return (
     <main>
-      <Form<ApiResponse>
-        fields={fields}
-        action={`${BASE_URL}api/posts`}
-        method="POST"
-        errorExtractor={extractErrorMsg}
-        // dataHandler={}
-        // successMsg=""
-        // navigateTo=""
-      />
+      {loading && <LoadingIndicator />}
+      {error && <p>{error}</p>}
+      {data && (
+        <Form<ApiResponse>
+          fields={fields}
+          action={`${BASE_URL}api/posts`}
+          method="POST"
+          errorExtractor={extractErrorMsg}
+          // dataHandler={}
+          // successMsg=""
+          // navigateTo=""
+        />
+      )}
     </main>
   );
+}
+
+/**
+ * Create form fields based on given `categoriesData` array
+ * @param {CategoryData[]} categoriesData - category data from API
+ * @returns array of field objects for `Form` component
+ */
+function getFields(categoriesData: CategoryData[]) {
+  return [
+    {
+      name: 'title',
+      label: 'Title',
+      type: 'text',
+      placeholder: 'title of post',
+      required: true,
+      colSpan: true,
+    },
+    {
+      name: 'category',
+      label: 'Category',
+      type: 'select',
+      colSpan: true,
+      selectOptions: [
+        {
+          value: '',
+          name: '-- Please select a category --',
+          selected: true,
+        },
+        // map each category to an obj w/ `value` & `name` properties
+        ...categoriesData.map((category) => ({
+          value: category._id,
+          name: category.name,
+        })),
+      ],
+    },
+    {
+      name: 'content',
+      label: 'Content',
+      type: 'editor',
+      placeholder: 'Enter post content here...',
+      required: true,
+      colSpan: true,
+    },
+    // tags
+    // displayImg
+    // publish checkbox
+  ];
 }
