fdc0d40ffab6a5e8aba2f922f0491bc8ebff24c3
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-06-17 19:01:34 -0400

    Factor 'PostFull' component out of PostPage

---
+++ src/components/PostFull.tsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import Image from './Image';
+import { PostData } from '../types';
+import { decode } from 'he'; // https://www.npmjs.com/package/he
+import { format } from 'date-fns/format'; // https://date-fns.org/v3.6.0/docs/format
+
+interface Props {
+  data: PostData;
+}
+
+export default function PostFull({ data }: Props) {
+  return (
+    <article className="flex flex-col gap-4">
+      <div>
+        <Image
+          src={data.displayImg?.url ? data.displayImg.url : ''}
+          alt=""
+          className="aspect-[3_/_2] mb-1"
+        />
+        {(data.displayImg?.attribution || data.displayImg?.source) && (
+          <small className="italic text-gray-500">
+            {'Image' +
+              (data.displayImg?.attribution
+                ? ` by ${data.displayImg.attribution}`
+                : '') +
+              (data.displayImg?.source
+                ? ` from ${data.displayImg.source}`
+                : '')}
+          </small>
+        )}
+      </div>
+      <div>
+        <h2>{decode(data.title)}</h2>
+        <div className="text-gray-500 flex gap-2 sm:gap-3 items-center mt-1">
+          <Link to={`/users/${data.user.slug}`}>
+            {decode(data.user.username)}
+          </Link>
+          <p>•</p>
+          <time dateTime={data.createdAt}>{format(data.createdAt, 'PPP')}</time>
+        </div>
+      </div>
+      <p className="">{decode(data.content)}</p>
+    </article>
+  );
+}
--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,12 +1,10 @@
-import { Link, useParams } from 'react-router-dom'; // https://reactrouter.com/en/6.23.0/hooks/use-params
+import { useParams } from 'react-router-dom'; // https://reactrouter.com/en/6.23.0/hooks/use-params
 import useFetch from '../utils/use-fetch';
 import Comments from '../components/Comments';
-import Image from '../components/Image';
 import LoadingIndicator from '../components/LoadingIndicator';
 import { BASE_URL } from '../config';
 import { ApiResponse, PostData } from '../types';
-import { decode } from 'he'; // https://www.npmjs.com/package/he
-import { format } from 'date-fns/format'; // https://date-fns.org/v3.6.0/docs/format
+import PostFull from '../components/PostFull';
 
 export default function PostPage() {
   const { postSlug = '' } = useParams();
@@ -26,29 +24,7 @@ export default function PostPage() {
   return (
     <main className="flex flex-col gap-8 max-w-screen-md">
       {loading && <LoadingIndicator />}
-      {data && (
-        <article className="flex flex-col gap-4">
-          <Image
-            src={data.displayImg?.url ? data.displayImg.url : ''}
-            alt=""
-            className="aspect-[3_/_2] mb-1"
-          />
-          <div>
-            <h2>{decode(data.title)}</h2>
-            <div className="text-gray-500 flex gap-2 sm:gap-3 items-center mt-1">
-              <Link to={`/users/${data.user.slug}`}>
-                {decode(data.user.username)}
-              </Link>
-              <p>•</p>
-              <time dateTime={data.createdAt}>
-                {format(data.createdAt, 'PPP')}
-              </time>
-            </div>
-          </div>
-          <p className="">{decode(data.content)}</p>
-        </article>
-      )}
-
+      {data && <PostFull data={data} />}
       <Comments postSlug={postSlug} />
     </main>
   );
