43de6f91312ac9772912ba23443abf94236f7769
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-07-06 12:15:57 -0400

    Clean up CmsPostCard component

--- src/components/CmsPostCard.tsx
+++ src/components/CmsPostCard.tsx
@@ -6,6 +6,7 @@ import {
   mdiChevronDown,
   mdiCircleEditOutline,
   mdiMinusCircleOutline,
+  mdiEyeCircleOutline,
 } from '@mdi/js';
 import { decode } from 'he'; // https://www.npmjs.com/package/he
 import { format } from 'date-fns'; // https://date-fns.org/v3.6.0/docs/format
@@ -16,13 +17,11 @@ import { ApiResponse, PostData } from '../types';
 
 interface Props {
   data: PostData;
-  editable?: boolean;
 }
 
 export default function CmsPostCard({ data }: Props) {
   const { fetchData } = useFetch<ApiResponse<PostData>>();
   const [isOpen, setIsOpen] = useState(false);
-  const postUrl = `/posts/${data.slug}`;
 
   function handleDelete() {
     if (window.confirm('Are you sure you want to delete this?')) {
@@ -44,27 +43,35 @@ export default function CmsPostCard({ data }: Props) {
   }
 
   return (
-    <div className="border border-gray-200 rounded-lg p-2 shadow">
+    <div className="border border-gray-200 rounded-lg p-2 shadow hover:shadow-md transition-all">
       <table className="w-full border-collapse">
+        <colgroup>
+          <col className="w-1/6" />
+          <col />
+        </colgroup>
+
         <thead>
           <tr>
             <th colSpan={2}>
-              <div className="flex justify-between">
-                <Link to={postUrl}>{decode(data.title)}</Link>
+              <div
+                className="flex justify-between cursor-pointer"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <span>{decode(data.title)}</span>
                 <button
                   className={'transition-all' + (isOpen ? ' rotate-180' : '')}
-                  onClick={() => setIsOpen(!isOpen)}
                 >
                   <Icon
                     path={mdiChevronDown}
                     color=""
-                    className="h-7 fill-gray-500 hover:fill-blue-600"
+                    className="h-7 fill-gray-500 hover:fill-blue-600 transition-all"
                   />
                 </button>
               </div>
             </th>
           </tr>
         </thead>
+
         {isOpen && (
           <tbody>
             <tr>
@@ -73,7 +80,7 @@ export default function CmsPostCard({ data }: Props) {
             </tr>
             <tr>
               <th scope="row">Tags</th>
-              <td>{data.tags ? data.tags.join(', ') : 'No tags'}</td>
+              <td>{data.tags?.length ? data.tags.join(', ') : 'No tags'}</td>
             </tr>
             <tr>
               <th scope="row">Status</th>
@@ -103,14 +110,26 @@ export default function CmsPostCard({ data }: Props) {
               <th scope="row">Image</th>
               <td>{data.displayImg?.url ? 'Has image' : 'No image'}</td>
             </tr>
+
             <tr>
               <td colSpan={2}>
                 <div className="flex gap-2">
+                  <Link
+                    to={`/posts/${data.slug}`}
+                    className="bg-blue-500 hover:bg-blue-600 text-white hover:text-white rounded px-3 py-2 w-fit flex items-center gap-2"
+                  >
+                    <Icon
+                      path={mdiEyeCircleOutline}
+                      color=""
+                      className="h-5 sm:h-6 fill-white"
+                    />
+                    <span className="-ml-[1px]">View</span>
+                  </Link>
                   <button className="bg-blue-500 hover:bg-blue-600 text-white rounded px-3 py-2 w-fit flex items-center gap-2 transition-all">
                     <Icon
                       path={mdiCircleEditOutline}
                       color=""
-                      className="h-6 fill-white"
+                      className="h-5 sm:h-6 fill-white"
                     />
                     <span>Edit</span>
                   </button>
@@ -121,7 +140,7 @@ export default function CmsPostCard({ data }: Props) {
                     <Icon
                       path={mdiMinusCircleOutline}
                       color=""
-                      className="h-6 fill-white"
+                      className="h-5 sm:h-6 fill-white"
                     />
                     <span>Delete</span>
                   </button>
