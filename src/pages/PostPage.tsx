8a9ba314aa9ffbc6c9f181cfbe21cc06e490d0dd
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-07-06 22:34:15 -0400

    Clean up PostPage

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,21 +1,19 @@
 import { useParams } from 'react-router-dom'; // https://reactrouter.com/en/6.23.0/hooks/use-params
 import useFetch from '../utils/use-fetch';
+import PostFull from '../components/PostFull';
 import Comments from '../components/Comments';
 import LoadingIndicator from '../components/LoadingIndicator';
 import { BASE_URL } from '../config';
 import { ApiResponse, PostData } from '../types';
-import PostFull from '../components/PostFull';
 
 export default function PostPage() {
   const { postSlug = '' } = useParams();
-  // const navigate = useNavigate();
   const {
     data: responseData,
     error,
     loading,
   } = useFetch<ApiResponse<PostData>>(`${BASE_URL}api/posts/${postSlug}`);
   const { data } = responseData ?? { data: null };
-  // console.log(responseData || loading);
 
   if (error) {
     throw new Error('A post with that name does not exist.');
